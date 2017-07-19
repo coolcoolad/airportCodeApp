d1d24d411c5e55d33d0b029beb354e90468299c7
Jie Yang <yajie@microsoft.com>
2017-07-19 16:03:20 +0800

    w

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 import os, sys, json, re
 sys.path.insert(0, os.path.abspath('../flaskPackages/'))
 from flask import Flask, request, Response
+import logging
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
 airportMap = {}
 featureMap = {}
 sep = '\s|-|\.|"|\'|\(|\)|/|,|&|airport'
@@ -28,7 +30,7 @@ def getAirportCode():
         else:
             raise Exception("fuzzy is wrong")
     except Exception, ex:
-        print ex.message
+        app.logger.info(ex.message)
         jsonMap['status'] = 1
         jsonMap['message'] = ex.message
     resp = Response(json.dumps(jsonMap, encoding='utf-8'))
@@ -38,13 +40,19 @@ def getAirportCode():
 @app.route('/', methods=['POST','GET'])
 @app.route('/hello', methods=['POST','GET'])
 def hello():
-    print request.data
-    print request.args
+    app.logger.info(request.data)
+    app.logger.info(request.args)
     return "hello"
 
 def initApp():
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)s - %(message)s')
+    logHandler = logging.FileHandler('info.log', encoding='utf-8')
+    logHandler.setFormatter(formatter)
+    app.logger.level = logging.INFO
+    app.logger.addHandler(logHandler)
+
     with open('airport_code.json') as fd:
-        jsonMap = json.load(fd,encoding='utf-8')
+        jsonMap = json.load(fd, encoding='utf-8')
         for row in jsonMap['list']:
             name = row['name'].lower()
             code = row['code'].upper()
@@ -55,6 +63,7 @@ def initApp():
 initApp()
 
 if __name__ == '__main__':
+    #app.debug = True
     #app.run(host='23.105.198.140',port=5001)
     app.run(host='10.172.211.107',port=5001)
 
